20aed3d3b52414984e79a890dae667f50faff958
2451052296viet@ou.edu.vn <2451052296viet@ou.edu.vn>
2026-06-16 16:19:27 +0700

    init project: singleton + abstract factory

---
+++ quizappv2/src/main/java/com/dht/quizappv2/PrimaryController.java
@@ -0,0 +1,48 @@
+package com.dht.quizappv2;
+
+import com.dht.utils.MyAlert;
+import com.dht.utils.themes.ThemeTypes;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class PrimaryController implements Initializable {
+    @FXML private ComboBox<ThemeTypes> cbThemes;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        this.cbThemes.setItems(FXCollections.observableArrayList(ThemeTypes.values()));
+    }
+    
+    public void manageQuestion(ActionEvent e) {
+        MyAlert.getInstance().showAlert("Coming soon...");
+    }
+    
+    public void practice(ActionEvent e) {
+        MyAlert.getInstance().showAlert("Coming soon...");
+    }
+    
+    public void exam(ActionEvent e) {
+        MyAlert.getInstance().showAlert("Coming soon...");
+    }
+
+    public void changeTheme(ActionEvent e) {
+        switch (this.cbThemes.getSelectionModel().getSelectedItem()) {
+            case DARK:
+                this.cbThemes.getScene().getRoot().getStylesheets().clear();
+                this.cbThemes.getScene().getRoot().getStylesheets().add(App.class.getResource("dark.css").toExternalForm());
+                break;
+            case LIGHT:
+                this.cbThemes.getScene().getRoot().getStylesheets().clear();
+                this.cbThemes.getScene().getRoot().getStylesheets().add(App.class.getResource("light.css").toExternalForm());
+                break;
+            default:
+                this.cbThemes.getScene().getRoot().getStylesheets().clear();
+                this.cbThemes.getScene().getRoot().getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
+        }
+    }
+}
